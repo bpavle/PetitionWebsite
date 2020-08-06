3cf0bb71d80709975f688f85f11318795c75cb85
Ach0n1 <acamicic@hotmail.com>
2020-08-06 15:32:05 +0200

    insert_organizer

--- get_complete_signatures.php
+++ get_complete_signatures.php
@@ -8,18 +8,33 @@ $rez=mysqli_query($link,$upit);
 
 while($podaci=mysqli_fetch_assoc($rez)){
 
-  $ime=$podaci['Name'];
-  $prezime=$podaci['Surname'];
-  $mail=$podaci['Email'];
-  $licna=$podaci['personal_id_number'];
-  $javniPotpis=$podaci['Public_Signature'];
-  $telefon=$podaci['Phone_Number'];
-  
+  $ime=$podaci['name'];
+  $prezime=$podaci['surname'];
+  $mail=$podaci['email'];
+  $licna=$podaci['id_number'];
+  $javniPotpis=$podaci['publish'];
+  $telefon=$podaci['phone_number'];
+  $komentar=$podaci['comment'];
+  $lokacija=$podaci['location_id'];
+  $broj_termina=$podaci['appointment_count'];
+  $termini=$podaci['appointments'];
+  $preuzet=$podaci['taken_over'];
+  $email_obavestenje=$podaci['email_notification'];
+
   if($javniPotpis==1){
       $javniPotpis='ДА';
   }
   else $javniPotpis='НЕ';
+
+  if($preuzet==1){
+    $preuzet='ДА';
+}
+else $preuzet='НЕ';
   
+if($email_obavestenje==1){
+  $email_obavestenje='ДА';
+}
+else $email_obavestenje='НЕ';
     
     echo<<<EOT
     <tr>
@@ -28,10 +43,11 @@ while($podaci=mysqli_fetch_assoc($rez)){
     <td><a href="mailto:$mail">$mail</a></td>
     <td>$telefon</td>
     <td>$licna</td>
-    <td>01/10/2019</td>
-    <td>10:42 AM</td>
+    <td>$lokacija</td>
     <td>$javniPotpis</td>
-    <td>Супер</td>
+    <td>$email_obavestenje</td>
+    <td>$preuzet</td>
+    <td>$komentar</td>
     <td><INPUT type="image" src="assets/update.png" value=""> </td>
     <td><INPUT type="image" src="assets/delete.png" value=""> </td>
   </tr>
