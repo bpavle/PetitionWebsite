f9926cb89b6679f9cccdff94e00a1512fbfbe0ef
Ach0n1 <acamicic@hotmail.com>
2020-08-10 13:24:51 +0200

    get_comments nema cirilice nigde

---
+++ get_comments.php
@@ -0,0 +1,29 @@
+
+
+
+<?php
+
+$link=mysqli_connect("localhost:3308", "root","","petition");
+
+$upit="SELECT * FROM sign;";
+
+$rez=mysqli_query($link,$upit);
+
+while($podaci=mysqli_fetch_assoc($rez)){
+
+  $ime=$podaci['name'];
+  $komentar=$podaci['comment'];
+    
+    echo<<<EOT
+    
+    <div class="comment">
+        <h4>$ime</h4>
+        $komentar
+      </div>
+      <hr />
+ 
+  EOT; 
+}
+
+mysqli_close($link);
+?>
